0be12dba48f134c359e14623ab1a8578c734837b
Trebor Huang <2300936257@qq.com>
2021-07-29 00:13:04 +0800

    Implement Implicit; Fix bug
    Now we have implicit lamdas, implicit applications and implicit function
    spaces. I found a bug involving WHNF computation -- it wasn't
    implemented along the textbook algorithm's lines, and instead ignored
    the case where multiple applications and lambdas lie on the spine. I
    carelessly invented it; now the code follows standard practice.

--- src/TT.hs
+++ src/TT.hs
@@ -9,7 +9,7 @@ type ConstName = String
 -- If terms and types are mutually recursively defined, the ABTCompatible class needs
 -- to use the forall extension. For simplicity, we define a big type containing both.
 data Term' = Set
-    | (:->) Term Term
+    | Fun Term Term Bool
     | Const ConstName
     | App Term Term
     | Lam Term
@@ -17,18 +17,18 @@ data Term' = Set
 type Term = ABT Term'
 type Type = Term
 instance (ABTCompatible Term') where
-    fallthrough f (m :-> n) = f m :-> f n
+    fallthrough f (Fun m n b) = Fun (f m) (f n) b
     fallthrough f (App m n) = App (f m) (f n)
     fallthrough f (Lam n) = Lam (f n)
     fallthrough _ a = a
 
-    collect f (m :-> n) = fromList [f m, f n]
+    collect f (Fun m n b) = fromList [f m, f n]
     collect f (App m n) = fromList [f m, f n]
     collect f (Lam n) = singleton $ f n
     collect _ m = empty
 
 allConstants :: Term -> Set ConstName
-allConstants (Node (m :-> n)) = allConstants m `union` allConstants n
+allConstants (Node (Fun m n b)) = allConstants m `union` allConstants n
 allConstants (Node (Const c)) = singleton c
 allConstants (Node (App m n)) = allConstants m `union` allConstants n
 allConstants (Node (Lam m)) = allConstants m
@@ -40,7 +40,7 @@ hasDuplicate [] = False
 hasDuplicate (x:xs) = x `elem` xs && hasDuplicate xs
 
 type Context = [(VarName, Type)]  -- ! Check distinctness
-type Telescope = Context
+type Telescope = [(VarName, Type, Bool)]
 -- Telescopes are cons, Contexts are snoc
 
 -- The following aux functions implement
@@ -48,7 +48,7 @@ type Telescope = Context
 -- iterated function spaces and function application
 funcContext :: Context -> Type -> Type
 funcContext [] t = t
-funcContext ((x, m):xs) t = funcContext xs (Node (m :-> Bind (abstract x t)))
+funcContext ((x, m):xs) t = funcContext xs (Node (Fun m (Bind (abstract x t)) True))
 
 substContext :: Context -> [Term] -> Term -> Term
 substContext [] [] = id
@@ -61,23 +61,23 @@ appContext t ((x, _):xs) = Node (App (appContext t xs) (FVar x))
 
 lamContext :: Context -> Term -> Term
 lamContext [] t = t
-lamContext ((x, m):xs) t = lamContext xs $ Node $ Lam $ abstract x t
+lamContext ((x, m):xs) t = lamContext xs $ Node $ Lam $ Bind $ abstract x t
 
 funcTelescope :: Telescope -> Type -> Type
 funcTelescope [] t = t
-funcTelescope ((x, m):xs) t = Node (m :-> Bind (abstract x (funcTelescope xs t)))
+funcTelescope ((x, m, p):xs) t = Node (Fun m (Bind (abstract x (funcTelescope xs t))) p)
 
 appTelescope :: Term -> Telescope -> Term
 appTelescope t [] = t
-appTelescope t ((x, _): xs) = appTelescope (Node $ App t $ FVar x) xs
+appTelescope t ((x, _, p): xs) = appTelescope (Node $ App t $ FVar x) xs
 
 lamTelescope :: Telescope -> Term -> Term
 lamTelescope [] t = t
-lamTelescope ((x, m):xs) t = Node $ Lam $ abstract x $ lamTelescope xs t
+lamTelescope ((x, m, b):xs) t = Node $ Lam $ Bind $ abstract x $ lamTelescope xs t
 
 substTelescope :: Telescope -> [Term] -> Term ->  Term
 substTelescope [] [] = id
-substTelescope ((x,a):xs) (s:ss) =  substitute s x . substTelescope xs ss
+substTelescope ((x,a,b):xs) (s:ss) =  substitute s x . substTelescope xs ss
 substTelescope _ _ = error "Length mismatch"
 
 contextDistinct :: Context -> Bool
@@ -113,24 +113,28 @@ data Constraint =
     deriving (Eq, Show)
 
 data UserExpr' =
-      ULam UserExpr
-    | UApp UserExpr UserExpr -- ! No beta allowed, prevent at parsing
+      ULam UserExpr Bool
+    | UApp UserExpr UserExpr Bool
     | UConst ConstName
     | USet
-    | UFun UserExpr UserExpr
+    | UFun UserExpr UserExpr Bool
     | Unknown
     deriving (Eq, Show)
 type UserExpr = ABT UserExpr'
 
+uappContext :: UserExpr -> Context -> UserExpr
+uappContext e [] = e
+uappContext e ((x,_):xs) = Node (UApp (uappContext e xs) (FVar x) True)
+
 instance (ABTCompatible UserExpr') where
-    fallthrough f (ULam x) = ULam $ f x
-    fallthrough f (UApp x y) = UApp (f x) (f y)
-    fallthrough f (UFun x y) = UFun (f x) (f y)
+    fallthrough f (ULam x b) = ULam (f x) b
+    fallthrough f (UApp x y b) = UApp (f x) (f y) b
+    fallthrough f (UFun x y b) = UFun (f x) (f y) b
     fallthrough f x = x
 
-    collect f (ULam x) = singleton $ f x
-    collect f (UApp x y) = fromList [f x, f y]
-    collect f (UFun x y) = fromList [f x, f y]
+    collect f (ULam x b) = singleton $ f x
+    collect f (UApp x y b) = fromList [f x, f y]
+    collect f (UFun x y b) = fromList [f x, f y]
     collect f _ = empty
 
 data WHNF = VariableHead {  -- arguments in natural order
@@ -139,12 +143,13 @@ data WHNF = VariableHead {  -- arguments in natural order
 } | ConstHead {
     constHead :: ConstName,
     arguments :: [Term]
-} deriving (Show, Eq)
+} | LamHead {lamHead :: Term} deriving (Show, Eq)
 
 forgetWHNF :: WHNF -> Term
 forgetWHNF w = case w of
     (VariableHead v xs) -> helper (FVar v) xs
     (ConstHead c xs) -> helper (Node $ Const c) xs
+    (LamHead l) -> Node (Lam (Bind l))
     where
         helper tm [] = tm
         helper tm (x:xs) = helper (Node (App tm x)) xs
@@ -156,8 +161,11 @@ data TypeCheckingFailures =
     | ConstAlreadyAssigned ConstName
     | ScopeCheckFailed ConstName Term
     | TypeInferenceFailed UserExpr
+    | TypeMismatch UserExpr Type
+    | CannotConvert Term Term
     | CannotConvertToFunction
-    | WHNFNotConvertible
+    | WHNFNotConvertible WHNF WHNF
+    | UnexpectedSyntax Term
     deriving (Show, Eq)
 
 type Elab a = ExceptT TypeCheckingFailures (StateT Signature Identity) a
@@ -218,25 +226,31 @@ lookUpConstant c = do
 
 checkType :: Context -> UserExpr -> Elab Type
 checkType ctx (Node USet) = return (Node Set)
-checkType ctx e@(Node (UFun e1 (Bind e2))) = do
+checkType ctx e@(Node (UFun e1 (Bind e2) b)) = (do
     t1 <- checkType ctx e1
     let x = fresh [e1, e2] (map fst ctx)
     t2 <- checkType ((x, t1) : ctx) (instantiate (FVar x) e2)
-    return $ Node (t1 :-> Bind (abstract x t2))
+    return $ Node (Fun t1 (Bind (abstract x t2)) b))
     `catchError` \x -> checkTerm ctx e (Node Set) `catchError` const (throwError x)
     -- rethrow error if backtracking failed to get more readable
 checkType ctx e = checkTerm ctx e (Node Set)
 
 checkTerm :: Context -> UserExpr -> Type -> Elab Term
-checkTerm ctx e0@(Node (ULam (Bind e))) t0@(Node (a :-> (Bind b))) = do
+checkTerm ctx e0@(Node (ULam (Bind e) p1)) t0@(Node (Fun a (Bind b) p2))
+  | p1 == p2 = do
     let x = fresh' (unions [freeVariables e, freeVariables a, freeVariables b])
     Node . Lam . Bind . abstract x <$>
       checkTerm ((x, a) : ctx) (instantiate (FVar x) e) (instantiate (FVar x) b)
-    `catchError` const (_checkTermFallBack ctx e0 t0)
+  | not p2 = checkTerm ctx (Node (ULam (Bind e0) False)) (Node (Fun a (Bind b) False))
+  | otherwise = throwError $ TypeMismatch e0 t0
 checkTerm ctx (Node Unknown) t = do
-    c <- gets freshConstant
-    addMeta c (funcContext ctx t)
-    return (appContext (Node (Const c)) ctx)
+    alpha <- gets freshConstant
+    addMeta alpha (funcContext ctx t)
+    return (appContext (Node (Const alpha)) ctx)
+checkTerm ctx e t@(Node (Fun a (Bind b) False)) =
+    _checkTermFallBack ctx e t
+    `catchError`
+    const (checkTerm ctx (Node (ULam (Bind e) False)) (Node (Fun a (Bind b) False)))
 checkTerm ctx e t = _checkTermFallBack ctx e t
 
 _checkTermFallBack ctx e t = do
@@ -257,49 +271,58 @@ inferType ctx (FVar x) = do
 inferType ctx (Node (UConst c)) = do
     s <- lookUpConstant c
     return (s, Node (Const c))
-inferType ctx (Node (UApp m n)) = do
+inferType ctx e@(Node (UApp m n b)) = do
     x <- inferType ctx m
     case x of
-        (Node (t1 :-> (Bind t2)), s) -> do
+        (Node (Fun t1 (Bind t2) b1), s) -> if b1 == b then do
             t <- checkTerm ctx n t1
             return (instantiate t t2, Node (App s t))
-        (_, s) -> throwError CannotConvertToFunction
+            else if b then do -- we need to clear out all the implicit lambdas
+                alpha <- gets freshConstant
+                addMeta alpha (funcContext ctx t1)
+                let implicitArgument = uappContext (Node (UConst alpha)) ctx
+                inferType ctx (Node (UApp (Node (UApp m implicitArgument False)) n b))
+            else throwError (TypeInferenceFailed e)
+        (_, s) -> throwError CannotConvertToFunction -- TODO later we need to account for this
 inferType ctx s = throwError $ TypeInferenceFailed s
 
 checkTypeConversion :: Context -> Type -> Type -> Elab [Constraint]
 checkTypeConversion ctx (Node Set) (Node Set) = return []
-checkTypeConversion ctx (Node (a1 :-> (Bind b1))) (Node (a2 :-> (Bind b2)))
-    = do  -- TODO in the future needs backtracking
+checkTypeConversion ctx f1@(Node (Fun a1 (Bind b1) plicity1)) f2@(Node (Fun a2 (Bind b2) plicity2))
+    | plicity1 == plicity2 = do  -- TODO in the future needs backtracking
         constraints <- checkTypeConversion ctx a1 a2
         let x = fresh [a1, a2, b1, b2] (map fst ctx)
         if null constraints then do
             checkTypeConversion ((x, a1):ctx) (instantiate (FVar x) b1) (instantiate (FVar x) b2)
         else do
+            -- !?
             p <- gets freshConstant
             addConst p
-                (funcContext ctx $ Node (a1 :-> Bind a2))
+                (funcContext ctx $ Node (Fun a1 (Bind a2) True))
                 (lamContext ctx $ Node (Lam (Bind (BVar 0))))
                 constraints
             constraints' <- checkTypeConversion ((x, a1):ctx)
                 (instantiate (Node (App (appContext (Node (Const p)) ctx) (FVar x))) b1)
                 (instantiate (Node (App (appContext (Node (Const p)) ctx) (FVar x))) b2)
             return $ constraints ++ constraints'
+    | otherwise = throwError $ CannotConvert f1 f2
 checkTypeConversion ctx t1 t2 = checkTermConversion ctx (Node Set) t1 t2
 
 checkTermConversion :: Context -> Type -> Term -> Term -> Elab [Constraint]
-checkTermConversion ctx (Node (a :-> (Bind b))) s t = do
+checkTermConversion ctx (Node (Fun a (Bind b) _)) s t = do  -- plicity does not matter
     let x = fresh [a,b,s,t] (map fst ctx)
     checkTermConversion ((x, a):ctx)
         (instantiate (FVar x) b)
         (Node (App s (FVar x)))
         (Node (App t (FVar x)))
 checkTermConversion ctx a s t = do
-    s' <- computeWHNF s
-    t' <- computeWHNF t
+    sig <- get
+    s' <- computeWHNF $ substituteAllConst sig s
+    t' <- computeWHNF $ substituteAllConst sig t
     checkWHNFConversion ctx a s' t'
 
 checkWHNFConversion :: Context -> Type -> WHNF -> WHNF -> Elab [Constraint]
-checkWHNFConversion ctx a' (VariableHead h ss) (VariableHead h' ts)
+checkWHNFConversion ctx a' w1@(VariableHead h ss) w2@(VariableHead h' ts)
     | h == h' && ss == ts = return []
     | h == h' && length ss == length ts = do
         case lookup h ctx of
@@ -311,28 +334,25 @@ checkWHNFConversion ctx a' (VariableHead h ss) (VariableHead h' ts)
                     else error "Type mismatch"  -- ! code unreachable, will remove
             _ -> throwError $ VariableNotFound h
     | otherwise =
-        throwError WHNFNotConvertible
+        throwError $ WHNFNotConvertible w1 w2
             where
                 seperateArguments :: Context -> Type -> Int -> Elab (Telescope, Type)
                 seperateArguments ctx a 0 = return ([], a)
-                seperateArguments ctx (Node (a :-> (Bind b))) n = do
+                seperateArguments ctx (Node (Fun a (Bind b) plicity)) n = do
                     let x = fresh ([]::[Term]) (map fst ctx)
                     (delta, t) <- seperateArguments ((x,a):ctx) (instantiate (FVar x) b) (n-1)
-                    return ((x,a):delta, t)
-                seperateArguments _ _ _ = throwError WHNFNotConvertible
+                    return ((x,a,plicity):delta, t)
+                seperateArguments _ _ _ = throwError $ WHNFNotConvertible w1 w2
 checkWHNFConversion ctx a f@(ConstHead c ss) t | f == t = return []
-    -- this is the important part.
-    -- it actually does all the unification work.
-    -- (currently it is very rudimentary)
                                                | otherwise =
     if distinctVariables ss then do
         s' <- computeNF (forgetWHNF t)
-        if all ((`elem` ss).FVar) (freeVariables s') then do
-            let expr = lamTelescope [(x, undefined) | FVar x <- ss] s'
+        if all ((`elem` ss).FVar) (freeVariables s') then (do
+            let expr = lamTelescope [(x, undefined, undefined) | FVar x <- ss] s'
             inScope c expr
             addAssignment c expr
-            return []
-            `catchError` const 
+            return [])
+            `catchError` const
                 (return [CTrm ctx a (forgetWHNF f) (forgetWHNF t)])
         else
             return [CTrm ctx a (forgetWHNF f) (forgetWHNF t)]
@@ -343,7 +363,7 @@ checkWHNFConversion ctx a f@(ConstHead c ss) t | f == t = return []
         distinctVariables [] = True
         distinctVariables ((FVar x) : xs) = distinctVariables xs && FVar x `notElem` xs
         distinctVariables _ = False
-checkWHNFConversion ctx a s t = throwError WHNFNotConvertible
+checkWHNFConversion ctx a s t = throwError $ WHNFNotConvertible s t
 
 computeNF :: Term -> Elab Term
 computeNF (Node (Const c)) = do
@@ -360,24 +380,19 @@ computeNF (Node (Lam (Bind m))) = do
     let x = fresh [m] []
     Node . Lam . Bind . abstract x <$> computeNF (instantiate (FVar x) m)
 computeNF (FVar x) = return (FVar x)
-computeNF _ = error "Unexpected Syntax!"
+computeNF t = throwError $ UnexpectedSyntax t
 
 computeWHNF :: Term -> Elab WHNF
-computeWHNF (Node (Const c)) = do
-    s <- gets (filter ((==c).getName))
-    case s of
-        DeclareConstraint _ ty tm g : _ | null g -> computeWHNF tm
-                                        | otherwise -> return $ ConstHead c []
-        DeclareEq _ ty tm : _ -> computeWHNF tm
-        DeclareType _ ty : _ -> return $ ConstHead c []
-        [] -> throwError $ ConstNotFound c
-computeWHNF (Node (App (Node (Lam (Bind m))) n)) = computeWHNF (instantiate n m)
+computeWHNF (Node (Const c)) = return (ConstHead c [])
 computeWHNF (Node (App m n)) = do
     m' <- computeWHNF m
-    return m'{arguments = arguments m' ++ [n]}  -- O(n^2). Might do the reversal at the end?
-computeWHNF (Node (Lam _)) = error "Lambdas should have been stripped already"
+    case m' of
+        LamHead l -> computeWHNF $ instantiate n l
+        _ -> return m'{arguments = arguments m' ++ [n]}  -- O(n^2). Might do the reversal at the end?
+computeWHNF (Node (Lam (Bind l))) = return $ LamHead l
 computeWHNF (FVar x) = return $ VariableHead x []
-computeWHNF _ = error "Unexpected Syntax!"
+computeWHNF t = throwError $ UnexpectedSyntax t
+
 
 checkSeqConversion :: Context -> [(VarName, Type, Term, Term)] -> Elab [Constraint]
 checkSeqConversion ctx [] = return []
@@ -393,10 +408,14 @@ checkSeqConversion ctx eqs'@((x, ty, t1, t2):eqs) = do
         return (c0 ++ c1)
         where snd4 (a,b,c,d) = b
 
-stripGuardedConstant :: Signature -> Signature
-stripGuardedConstant = map strip
-    where
-        strip :: SignatureSnippet -> SignatureSnippet
-        strip (DeclareConstraint c t tm g) | null g = DeclareEq c t tm
-                                           | otherwise = DeclareType c t
-        strip s = s
+substituteAllConst :: Signature -> Term -> Term
+substituteAllConst sig (Node (Const c)) =
+    case head $ filter ((==c).getName) sig of
+        DeclareEq _ _ t -> t
+        _ -> Node (Const c)
+substituteAllConst sig (Node Set) = Node Set
+substituteAllConst sig (Node (Fun m n b)) = Node (Fun (substituteAllConst sig m) (substituteAllConst sig n) b)
+substituteAllConst sig (Node (App m n)) = Node (App (substituteAllConst sig m) (substituteAllConst sig n))
+substituteAllConst sig (Node (Lam m)) = Node (Lam (substituteAllConst sig m))
+substituteAllConst sig (Bind m) = Bind (substituteAllConst sig m)
+substituteAllConst sig v = v
--- test/Spec.hs
+++ test/Spec.hs
@@ -13,7 +13,7 @@ instance (ABTCompatible NodeType) where
 
 test :: (Eq a, Show a) => a -> a -> String
 test m n | m == n = "Passed."
-         | otherwise = "Failed!\n" ++ show m ++ " is not equal to " ++ show n
+         | otherwise = error $ "Failed!\n\n" ++ show m ++ "\n=====is not equal to=====\n" ++ show n
 
 -- abstract and instantiate
 test1 = test (abstract "x" (Node (LamTest (Bind (FVar "x"))))) (Node (LamTest (Bind (BVar 1))))
@@ -24,15 +24,20 @@ test2 = test (instantiate t m) res
       res = Node (LamTest (Bind (Node (AppTest (Node (AppTest (FVar "x") (Node (LamTest (Bind (BVar 0)))))) (BVar 0)))))
 
 -- Test 3 to 5 follows the linked paper.
+infixr ->:
+(->:) x y = Node (Fun x (Bind y) True)
+infixr ->?
+(->?) x y = Node (Fun x (Bind y) False)
+
 sig1 = reverse [
     DeclareType "Nat" (Node Set),
     DeclareType "0" (Node (Const "Nat")),
-    DeclareEq "id" (Node (Node Set :-> Bind (Node (BVar 0 :-> Bind (BVar 1))))) (Node (Lam (Bind (Node (Lam (Bind (BVar 0))))))),
+    DeclareEq "id" (Node Set ->: ( BVar 0 ->: BVar 1)) (Node (Lam (Bind (Node (Lam (Bind (BVar 0))))))),
     DeclareType "alpha" (Node Set)
     ]
 
 elab1 :: Elab Term
-elab1 = checkTerm [] (Node (UApp (Node (UApp (Node (UConst "id")) (Node Unknown))) (Node (UConst "0")))) (Node (Const "alpha"))
+elab1 = checkTerm [] (Node (UApp (Node (UApp (Node (UConst "id")) (Node Unknown) True)) (Node (UConst "0")) True)) (Node (Const "alpha"))
 
 res1 = (Right (Node (App (Node (App (Node (Const "id")) (Node (Const "c")))) (Node (Const "0")))),
   [
@@ -45,7 +50,7 @@ test3 = test (runElab elab1 sig1) res1
 sig2 = reverse [
     DeclareType "Nat" (Node Set),
     DeclareType "0" (Node (Const "Nat")),
-    DeclareType "suc" (Node (Node (Const "Nat") :-> Bind (Node (Const "Nat")))),
+    DeclareType "suc" (Node (Const "Nat") ->: Node (Const "Nat")),
     DeclareType "caseNat" (
           ({-P-} Node (Const "Nat") ->: Node Set)
       ->: Node (App (BVar 0 {-P-}) (Node (Const "0")))
@@ -54,48 +59,23 @@ sig2 = reverse [
       ->: Node (App (BVar 3 {-P-}) (BVar 0 {-n-}))
       )
     ]
-    where
-      infixr ->:
-      (->:) x y = Node (x :-> Bind y)
 
 elab2 = checkTerm [] (Node (UApp
   (Node (UApp
     (Node (UApp
       (Node (UConst "caseNat"))
-      (Node Unknown)))
-    (Node (UConst "0"))))
-  (Node (ULam (Bind (BVar 0))))))
-  (Node (Node (Const "Nat") :-> Bind (Node (Const "Nat"))))
+      (Node Unknown) True))
+    (Node (UConst "0")) True))
+  (Node (ULam (Bind (BVar 0)) True)) True))
+  (Node (Const "Nat") ->: Node (Const "Nat"))
 
-res2 = (
-  Right (Node (Const "```c")),
+res2 = (Right (Node (Const "```c")),
   [
-    DeclareConstraint
-      "```c"
-      (Node ((:->) (Node (Const "Nat")) (Bind (Node (Const "Nat")))))
-      (Node (App (Node (App (Node (App (Node (Const "caseNat")) (Node (Const "c")))) (Node (Const "`c")))) (Node (Lam (Bind (Node (App (Node (Const "``c")) (BVar 0))))))))
-      [
-        CTrm
-          [("x", Node (Const "Nat"))]
-          (Node Set)
-          (Node (Const "Nat"))
-          (Node (App (Node (Const "c")) (FVar "x")))
-      ],
-    DeclareConstraint
-      "``c"
-      (Node ((:->) (Node (Const "Nat")) (Bind (Node (App (Node (Const "c")) (Node (App (Node (Const "suc")) (BVar 0))))))))
-      (Node (Lam (BVar 0)))
-      [CTrm [("x", Node (Const "Nat"))] (Node Set) (Node (App (Node (Const "c")) (Node (App (Node (Const "suc")) (FVar "x"))))) (Node (Const "Nat"))],
-    DeclareConstraint
-      "`c"
-      (Node (App (Node (Const "c"))
-      (Node (Const "0"))))
-      (Node (Const "0"))
-      [CTrm [] (Node Set) (Node (App (Node (Const "c")) (Node (Const "0")))) (Node (Const "Nat"))],
-    DeclareType
-      "c"
-      (Node ((:->) (Node (Const "Nat")) (Bind (Node Set))))
-    ] ++ sig2)
+    DeclareConstraint "```c" (Node (Const "Nat") ->: Node (Const "Nat")) (Node (App (Node (App (Node (App (Node (Const "caseNat")) (Node (Const "c")))) (Node (Const "`c")))) (Node (Lam (Bind (Node (App (Node (Const "``c")) (BVar 0)))))))) [CTrm [("x",Node (Const "Nat"))] (Node Set) (Node (Const "Nat")) (Node (App (Node (Const "c")) (FVar "x")))],
+    DeclareConstraint "``c" (Node (Const "Nat") ->: Node (App (Node (Const "c")) (Node (App (Node (Const "suc")) (BVar 0))))) (Node (Lam (Bind (BVar 0)))) [CTrm [("x",Node (Const "Nat"))] (Node Set) (Node (App (Node (Const "c")) (Node (App (Node (Const "suc")) (FVar "x"))))) (Node (Const "Nat"))],
+    DeclareConstraint "`c" (Node (App (Node (Const "c")) (Node (Const "0")))) (Node (Const "0")) [CTrm [] (Node Set) (Node (App (Node (Const "c")) (Node (Const "0")))) (Node (Const "Nat"))],
+    DeclareType "c" (Node (Const "Nat") ->: Node Set)
+  ] ++ sig2)
 
 test4 = test (runElab elab2 sig2) res2
 
@@ -103,10 +83,9 @@ sig3 = reverse [
     DeclareType "Nat" (Node Set),
     DeclareType "0" (Node (Const "Nat")),
     DeclareEq "coerce" -- \F x -> x  ::  (F : Nat -> Set) -> F 0 -> F 0
-      (Node
-        (Node (Node (Const "Nat") :-> Bind (Node Set))
-          :-> Node (Node (App (BVar 0) (Node (Const "0")))
-          :-> Node (App (BVar 1) (Node (Const "0"))))))
+      ((Node (Const "Nat") ->: Node Set)
+          ->: (Node (App (BVar 0) (Node (Const "0")))
+          ->: Node (App (BVar 1) (Node (Const "0")))))
       (Node (Lam (Bind (Node (Lam (Bind (BVar 0)))))))
   ]
 
@@ -117,11 +96,10 @@ elab3 = checkTerm []
       (Node (UApp
         (Node (UApp
           (Node $ UConst "coerce")
-          (Node Unknown)))
-        (BVar 0)))))))
-  (Node (
-    Node (Node (Const "Nat") :-> Bind (Node (Const "Nat")))
-    :-> Bind (Node (Const "Nat"))))
+          (Node Unknown) True))
+        (BVar 0) True)) True)) True))
+  ((Node (Const "Nat") ->: Node (Const "Nat"))
+    ->: Node (Const "Nat"))
 
 test5 = test (runElab elab3 sig3)
   (Left (TypeInferenceFailed
@@ -131,26 +109,88 @@ test5 = test (runElab elab3 sig3)
         (Node (UApp
           (Node (UApp
             (Node (UConst "coerce"))
-            (Node Unknown)))
-          (BVar 0)))))))), sig3)
+            (Node Unknown) True))
+          (BVar 0) True)) True)) True))), sig3)
 
-{-
-The following is an example mentioned by ice1000
+typeFlip = Node Set ->? (BVar 0 ->: BVar 1 ->: BVar 2) ->: BVar 1 ->: BVar 2 ->: BVar 3
 
-test : (a : _) (B : Set) (b : B) -> a ≡ b
-test a B b = refl
+elaboratedFlip = Node (Lam (Bind
+  (Node (Lam (Bind
+    (Node (Lam (Bind
+      (Node (Lam (Bind
+        (Node (App
+          (Node (App
+            (BVar 2)
+            (BVar 0)))
+          (BVar 1))))))))))))))
 
-Where
+termFlip = Node (ULam (Bind -- A
+  (Node (ULam (Bind -- F
+    (Node (ULam (Bind -- a
+      (Node (ULam (Bind -- b
+        (Node (UApp
+          (Node (UApp
+            (BVar 2)
+            (BVar 0) True))
+          (BVar 1) True)))
+          True)))
+          True)))
+          True)))
+          False)
 
-_≡_ : {A : Set} -> A -> A -> Set
-refl : {A : Set}{a : A} -> a ≡ a
+termFlip' = Node (ULam (Bind -- F
+    (Node (ULam (Bind -- a
+      (Node (ULam (Bind -- b
+        (Node (UApp
+          (Node (UApp
+            (BVar 2)
+            (BVar 0) True))
+          (BVar 1) True)))
+          True)))
+          True)))
+          True) -- \A is implicit
 
-This will be done after we have implicit binders
+termFlip'' = Node (ULam (Bind -- F
+    (Node (ULam (Bind -- a
+      (Node (ULam (Bind -- b
+        (Node (UApp
+          (Node (UApp
+            (Node (UApp
+              (Node (UConst "flip"))
+              (BVar 2) True))
+            (BVar 1) True))
+          (BVar 0) True)))
+          True)))
+          True)))
+          True)
 
--}
+elaboratedFlip'' = Node (Lam (Bind (Node (Lam (Bind (Node (Lam (Bind (Node (Lam (Bind (Node (App (Node (App (Node (App (Node (App (Node (Const "flip")) (Node (App (Node (App (Node (App (Node (App (Node (Const "c")) (BVar 3))) (BVar 2))) (BVar 1))) (BVar 0))))) (BVar 2))) (BVar 1))) (BVar 0))))))))))))))
+
+elab4 = checkTerm [] termFlip typeFlip
+elab5 = checkTerm [] termFlip' typeFlip
+elab6 = checkTerm [] termFlip'' typeFlip
+
+test6 = test (runElab elab4 []) (Right elaboratedFlip, [])
+
+test7 = test (runElab elab5 []) (Right elaboratedFlip, [])
+
+test8 = test (runElab elab6 [DeclareEq "flip" typeFlip elaboratedFlip]) (Right elaboratedFlip'',
+  [
+    DeclareEq "c" (Node Set  -- (A : Set) -> (A -> A -> A) -> A -> A -> Set
+      ->: (BVar 0 ->: BVar 1 ->: BVar 2)
+      ->: BVar 1
+      ->: BVar 2
+      ->: Node Set)
+
+      (Node (Lam (Bind
+        (Node (Lam (Bind
+          (Node (Lam (Bind
+            (Node (Lam (Bind
+              (BVar 3))))))))))))), -- \ A f b c -> A
+    DeclareEq "flip" typeFlip elaboratedFlip])
 
 tests :: [String]
-tests = [test1, test2, test3, test4, test5]
+tests = [test1, test2, test3, test4, test5, test6, test7, test8]
 
 main :: IO ()
 main = putStrLn (intercalate "\n" tests)
